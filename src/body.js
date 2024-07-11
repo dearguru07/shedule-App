f04b0b509b1f675dec719c517157c2c036821c8a
dearguru07 <pangalaguruyadav101@gmail.com>
2024-07-12 00:16:27 +0530

    'new'

--- src/body.js
+++ src/body.js
@@ -1,38 +1,45 @@
-import { useEffect, useState } from "react"
-const Card=(props)=>{
-    return <div className="card">
-      <img className="img" src={"https://source.unsplash.com/1600x800/?"+props.data.meetImg}></img>
+import { useEffect, useState } from "react";
+const Card = (props) => {
+  return (
+    <div className="card">
+      <img
+        className="img"
+        src={"https://picsum.photos/200/300/?" + props.data.meetImg}
+      ></img>
       <h3 className="list">{props.data.meetName}</h3>
       <h4 className="list">{props.data.meetTime}</h4>
       <p className="list">{props.data.meetInfo}</p>
     </div>
-}
-const Body=()=>{
-    const [meets,setmeets]=useState([])
- async function meetData(){
-    const Data=await fetch("https://shedlule-default-rtdb.firebaseio.com/newapp.json")
-    const originalData=await Data.json()
-    console.log(originalData)
-    const array=[]
-    for(let key in originalData){
-        if (originalData.hasOwnProperty(key)){
-            array.push(originalData[key])
-        }
+  );
+};
+const Body = () => {
+  const [meets, setmeets] = useState([]);
+  async function meetData() {
+    const Data = await fetch(
+      "https://shedlule-default-rtdb.firebaseio.com/newapp.json"
+    );
+    const originalData = await Data.json();
+    console.log(originalData);
+    const array = [];
+    for (let key in originalData) {
+      if (originalData.hasOwnProperty(key)) {
+        array.push(originalData[key]);
+      }
     }
-    setmeets(array)
-}
-useEffect(()=>{
-    meetData()
-},[])
-    return <div>
-        <h2 className="center">Welcome back to all your meetings</h2>
-        <div className="body">
-     {
-    meets.map((x)=>{
-    return <Card data={x}/>
-    })
-     }
+    setmeets(array);
+  }
+  useEffect(() => {
+    meetData();
+  }, []);
+  return (
+    <div>
+      <h2 className="center">Welcome back to all your meetings</h2>
+      <div className="body">
+        {meets.map((x) => {
+          return <Card data={x} />;
+        })}
+      </div>
     </div>
-    </div>
-   }
+  );
+};
 export default Body;
